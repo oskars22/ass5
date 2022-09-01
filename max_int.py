1e58061094fa249fb16e289e7a424a31f208966e
oskars22 <oskars22@ru.is>
2022-09-01 10:02:45 +0000

    Minor change

--- max_int.py
+++ max_int.py
@@ -3,8 +3,8 @@
 # Ef integerinn er stærri eða jafn stór, þá er innslegni integerinn hinn nýji stærsti integer.
 # Notandinn er spurður aftur þangað til hann slær inn neikvæða tölu og þá endar forritið.
 
-max_int = 0
 num_int = int(input("Input a number: "))
+max_int = 0
 
 while num_int >= 0:
     if num_int >= max_int:
